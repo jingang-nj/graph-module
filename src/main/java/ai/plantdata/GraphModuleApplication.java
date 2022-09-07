f6030d1e34ece52787870951d5e08ff7b0d92ddc
李金钢 <1143745110@qq.com>
2022-09-07 18:34:51 +0800

    初始化

---
+++ src/main/java/ai/plantdata/GraphModuleApplication.java
@@ -0,0 +1,13 @@
+package ai.plantdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphModuleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ai/plantdata/common/struct/BeanStruct.java
@@ -0,0 +1,17 @@
+package ai.plantdata.common.struct;
+
+
+import org.mapstruct.Mapper;
+
+/**
+ * bean转换
+ *
+ * @author DingHao
+ * @since 2021/2/23 22:16
+ */
+@Mapper(componentModel = "spring")
+public interface BeanStruct {
+
+
+
+}
